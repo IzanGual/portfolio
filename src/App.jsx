b48ea16f66cd10edcd963089bce16520aa22e82e
IzanGual <iesvda.izamar@gmail.com>
2025-09-24 11:11:43 +0200

    feat: videoBackground demo finished and working

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <h1>Hello izan</h1>
-    </>
+    <div className="video-background">
+      <video autoPlay muted loop className="video">
+        <source src="/videos/videoBackgroundDark.mp4" type="video/mp4" />
+        Tu navegador no soporta videos.
+      </video>    
+      </div>
   )
 }
 
